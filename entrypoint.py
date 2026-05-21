00547f6110f4599cf193c6a0b983976e120c6d47
Sam Chiang <SamChiang@gmail.com>
2026-05-21 20:59:48 +0800

    chore: remove leftover entrypoint.py

--- entrypoint.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import subprocess
-
-print("Starting gunicorn on port 5000")
-subprocess.run([
-    "gunicorn", "--bind", "0.0.0.0:5000", "--workers", "2", "--threads", "4", "app:app"
-])
